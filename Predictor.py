4becf75a4d537149c798b77650cef51b6d2a29df
Jacob Baginski Doar <jjbagins@ucsc.edu>
2019-11-26 14:08:36 -0800

    Created Predictor.py and layed out pseudo code

---
+++ Predictor.py
@@ -0,0 +1,36 @@
+# Jacob Baginski Doar
+# 11/22/2019
+# Predictor.py
+
+# Implimentation of a neural network used to predict ratings on yelp reviews
+
+# Run in the command line with the format
+# python DataExtraction.py File -Mode
+# Where mode can be train or test
+
+import json
+import re
+
+# Take in an instance from a data set
+# Return an array that contains all extracted features
+def extractFeatures(instance):
+    return
+    
+def main():
+    # Read in data
+    
+    # IF TRAIN
+    # Extract features
+    # Run back propogation on training data
+    # This section will use tensor flow stuff
+    
+    # ELSE TEST
+    # Extract features
+    # Run prediction on each instance
+    # Record data and produce similar statistics to what was
+    #   we did for the logistic regression assignment
+    # Print results
+    return
+    
+main()
+
