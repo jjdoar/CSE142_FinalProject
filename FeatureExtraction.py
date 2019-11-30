e218cc844bc1070fd30c1be2a72d4590c714764a
iankirkm <58239320+iankirkm@users.noreply.github.com>
2019-11-30 14:57:29 -0800

    Add files via upload
    Added "!" counter, "$" counter, length, and all caps counter

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -4,19 +4,25 @@ import csv
 
 keywords = None
 
-# Returns a vector of occurrences of words in keywords
+# Returns a vector of occurrences of words in keywords and some other special features
 # Ex: keywords = ["good", "bad", "okay"]
-#     entry = "the food was good and the prices were good too, but the service was only okay"
-#     wordData = [2, 0, 1]
+#     entry = "the food was good for a good price of $10, but the service was only okay. DEFINITELY worth it I would say"
+#     wordData = [number of !, number of $, length in words, number of all caps, number of "good", number of "bad", number of "okay"]
+#     wordData = [0, 1, 22, 1, 2, 0, 1]
 def getVector(entry):
-    wordData = [0] * len(keywords)
+    wordData = [0] * (len(keywords) + 4)
     text = entry["text"]
+    wordData[0] = text.count("!") # number of exclamation points
+    wordData[1] = text.count("$") # number of $ signs
     text = re.findall(r"\w+", entry["text"])
+    wordData[2] = len(text) # how many words in review
     for w in text:
+        if w.isupper() and len(w) != 1 and (not any(char.isdigit() for char in w)): # if word is all caps, not a single letter, and not numeric. note: might want to change not numeric detection
+            wordData[3] += 1 # increase all caps feature
         w = w.lower()
         for i, k in enumerate(keywords):
             if w == k[0]:
-                wordData[i] += 1
+                wordData[i + 4] += 1
                 break
     return wordData
 
